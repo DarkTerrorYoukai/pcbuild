fba8c916b2b8dbf490e5c761c087f2cf338e69c3
JiPyYes <jipyworks@gmail.com>
2026-04-23 17:00:27 +0300

    header-design 1.0.1 | added fonts

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,18 @@
 </script>
 
 <template>
+    <div class="test1">jetbrains</div>
+    <div class="test2">jura</div>
     <router-link :to="{name: 'build' }">test</router-link>
     <router-view></router-view>
 </template>
 
 <style scoped>
+.test1 {
+    font-family: 'JetBrains_Mono';
+}
 
+.test2 {
+    font-family: 'Jura';
+}
 </style>
